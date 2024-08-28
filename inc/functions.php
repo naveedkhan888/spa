667e6c994900ff6a438c0a347c89240f5af84627
naveedkhan888 <88194719+naveedkhan888@users.noreply.github.com>
2024-08-28 15:25:52 +0500

    sdfdf

--- inc/functions.php
+++ inc/functions.php
@@ -130,7 +130,7 @@ if ( ! function_exists( 'spalisho_default_second_font' ) ) {
     function spalisho_default_second_font() {
         $customizer_defaults = json_encode(
             array(
-                'font' => 'Spectral',
+                'font' => 'Bitter',
                 'regularweight' => '100,200,300,400,500,600,700,800,900, regular, italic',
                 'category' => 'serif'
             )
