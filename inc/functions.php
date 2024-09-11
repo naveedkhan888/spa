dad1ac46d6c21f6c0887b5905465b864030d60a5
naveedkhan888 <88194719+naveedkhan888@users.noreply.github.com>
2024-09-11 14:46:47 +0500

    sdfsdf

--- inc/functions.php
+++ inc/functions.php
@@ -130,9 +130,9 @@ if ( ! function_exists( 'spalisho_default_second_font' ) ) {
     function spalisho_default_second_font() {
         $customizer_defaults = json_encode(
             array(
-                'font' => 'Cormorant Garamond',
-                'regularweight' => '300,400,500,600,700, regular, italic',
-                'category' => 'serif'
+                'font' => 'Bebas Neue',
+                'regularweight' => 'regular',
+                'category' => 'display'
             )
         );
 
